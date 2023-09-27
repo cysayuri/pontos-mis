5241ece0c1d49f1304af0f4c01a2f16bdcf23b4a
Cyntia Sayuri <cy.sayuri@hotmail.com>
2023-09-27 01:40:46 -0300

    Select Cidades - Inicio

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,7 +1,45 @@
+/*-- ------- Botão ------- --*/
+
 const filterBtn = document.querySelector('.programacao__filter-btn')
 const filterSelect = document.querySelector('.programacao__filter-select')
 
 filterBtn.addEventListener('click', () => {
     filterBtn.classList.add('disabled')
     filterSelect.classList.add('active')
+})
+
+
+
+
+
+
+/*-- ------- Mapa ------- --*/
+
+let programMap = document.querySelectorAll('.programacao__map-cores svg')
+let programItem = document.querySelectorAll('.programacao__list-item')
+
+programMap.forEach(elem => {
+    elem.addEventListener('click', () => {
+        filterBtn.classList.remove('disabled')
+        filterSelect.classList.remove('active')
+        
+        programMap.forEach(elem => {
+            elem.classList.remove('active')
+        })
+        
+        programItem.forEach(idx => {
+            idx.classList.remove('disabled')
+        })
+    
+        mapId = elem.getAttribute('id')
+
+        programItem.forEach(idx => {
+            itemId = idx.getAttribute('id')
+
+            if (!(mapId == ('mapa-' + itemId))) {
+                elem.classList.add('active')
+                idx.classList.add('disabled')
+            }
+        })
+    })
 })
